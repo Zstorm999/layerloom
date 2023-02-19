4be61d70cc85106a7d3001c3218e462562029be7
Zstorm999 <46458357+Zstorm999@users.noreply.github.com>
2023-02-19 18:01:53 +0100

    druid tutorial code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,47 @@
-use druid::{widget::Label, AppLauncher, Widget, WindowDesc};
+use druid::{
+    widget::{Button, Container, Flex, Label, LensWrap, List, Split, TextBox},
+    AppLauncher, Color, Data, Lens, Widget, WindowDesc,
+};
+use im::{vector, Vector};
 
-fn build_ui() -> impl Widget<()> {
-    Label::new("Hello World")
+#[derive(Clone, Data, Lens)]
+struct TodoList {
+    items: Vector<String>,
+    next_item: String,
+}
+
+// create the widget tree
+fn build_ui() -> impl Widget<TodoList> {
+    Split::columns(
+        Container::new(
+            // Dynamic list of Widgets
+            LensWrap::new(
+                // actual list is processed here
+                List::new(|| Label::dynamic(|data, _| format!("List item: {data}"))),
+                TodoList::items,
+            ),
+        )
+        .border(Color::grey(0.6), 2.0),
+        Container::new(
+            Flex::column()
+                .with_flex_child(
+                    // new item button
+                    Button::new("Add item").on_click(|_, data: &mut TodoList, _| {
+                        if !data.next_item.is_empty() {
+                            data.items.push_back(data.next_item.clone());
+                            data.next_item = String::new();
+                        }
+                    }),
+                    1.0,
+                )
+                .with_flex_child(
+                    // text zone for new items
+                    LensWrap::new(TextBox::new(), TodoList::next_item),
+                    1.0,
+                ),
+        )
+        .border(Color::grey(0.6), 2.0),
+    )
 }
 
 fn main() {
@@ -9,7 +49,17 @@ fn main() {
         .window_size((600.0, 400.0))
         .title("First Druid app");
 
-    let initial_data = ();
+    let initial_data = TodoList {
+        items: vector![
+            "first item".into(),
+            "second item".into(),
+            "third item".into(),
+            "fourth item".into(),
+            "foo".into(),
+            "bar".into(),
+        ],
+        next_item: String::new(),
+    };
 
     AppLauncher::with_window(main_window)
         .launch(initial_data)
